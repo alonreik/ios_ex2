32e93f98097de55fdf8ada62a7dedfa995c6086a
alonreik <reik@cherrypick-consulting.com>
2021-03-15 16:27:05 +0200

    Added cards and buttons (without functionalities) to the UI.

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Properties
+    let game = SetGame()
+    
+    
+    
+    
+    
+    // Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
