cccc3ee6553fff9d5b77d9a6367eac31c7a5cf80
alonreik <reik@cherrypick-consulting.com>
2021-03-15 10:43:16 +0200

    Defined a "SetCard" struct.

---
+++ Set/SetCard.swift
@@ -0,0 +1,25 @@
+//
+//  SetCard.swift
+//  Set
+//
+//  Created by Alon Reik on 15/03/2021.
+//
+
+import Foundation
+
+struct SetCard
+{
+    // Properties
+    let shapeType: Int
+    let shapesNum: Int
+    let shapeFilling: Int
+    
+    var matched = false
+    
+    // Initiators
+    
+    
+    
+    // Methods
+    
+}
