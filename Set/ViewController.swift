e55c05ae1669d7d6721b71239c4013bb17e304e7
alonreik <reik@cherrypick-consulting.com>
2021-03-16 13:17:54 +0200

    linked all buttons to the touchCard function.

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -18,8 +18,8 @@ class ViewController: UIViewController {
     // Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        // My additions for this function:
         setViewForGameBeginning()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func dealButtonPressed(_ sender: UIButton) {
@@ -29,6 +29,9 @@ class ViewController: UIViewController {
     @IBAction func newGamePressed(_ sender: UIButton) {
     }
     
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+    }
     
     
     // Private Methods
