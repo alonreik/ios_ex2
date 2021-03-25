7542b6d2abfda0212aa230c75970805a0f1df807
alonreik <reik@cherrypick-consulting.com>
2021-03-25 11:12:25 +0200

    committing before trying to create a canvas for the openCards

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -12,33 +12,56 @@ class SetCardView: UIView
     /* ---- */
     
     
+    // possible number of shapes in a card:
     // 1, 2, 3
-    var numberOfShapes = 1 {
+    var numberOfShapes = 3 {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     } // is needs layout necessary?
     
+    // possible shapes:
     // "▲", "●", "■"
     var shape = "▲" {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
-    
-    var color: UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) {
+
+    // possible colors:
+    // #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+    // #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    // #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    var color: UIColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1) {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
     
-    var filling = "" {
+    // possible fillings:
+    // striped, outline, full
+    var filling = "full" {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
     
-    
-    private func attriubtedStringForCard() -> NSAttributedString? {
+    //
+    private func attriubtedStringForCard(fontSize: CGFloat) -> NSAttributedString? {
+//        private func attriubtedStringForCard(fontSize: CGFloat) -> NSAttributedString? {
+
+        // create a preffered font with the given size.
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+
+        // The line below makes the font size adjust to people changing display settings on Iphone.
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+
+        // This way we set a centered text (paragraph style)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        
+        
         // Different filling types for cards get different alpha value for coloring.
         let alpha = (filling == "striped") ? CGFloat(0.15): CGFloat(1.0)
         let attributes: [NSAttributedString.Key: Any] = [
             // different filling types for cards get different .strokeWidth values.
             .strokeWidth: (filling == "outline") ? 5.0 : -5.0,
             .strokeColor: color,
-            .foregroundColor: color.withAlphaComponent(alpha)
+            .foregroundColor: color.withAlphaComponent(alpha),
+            .paragraphStyle: paragraphStyle,
+            .font: font
             ]
         var string = ""
         for _ in 0..<numberOfShapes {
@@ -50,12 +73,18 @@ class SetCardView: UIView
     
     /* ---- */
     
-    /*
+    /* */
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+        // todo - figure out font size
+        guard let attributedString = attriubtedStringForCard(fontSize: CGFloat(10.0)) else {
+            print("failed the let")
+            return
+        }
 
+        // todo - figure out inset by
+        let cardRect = bounds.insetBy(dx: 0, dy: 20)
+        attributedString.draw(in: cardRect)
+    }
 }
