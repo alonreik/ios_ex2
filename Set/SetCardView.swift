de142531cab39bdf681e39cb10e9907dddf05ca0
alonreik <reik@cherrypick-consulting.com>
2021-03-25 15:17:42 +0200

    finished adjusting the setCardView before starting to work on the controller.

--- Set/SetCardView.swift
+++ Set/SetCardView.swift
@@ -9,39 +9,81 @@ import UIKit
 
 class SetCardView: UIView
 {
-    /* ---- */
+    /* -- enums  -- */
+    
+    enum Shape {
+        case triangle
+        case circle
+        case square
+    }
+    enum Filling: String {
+        case full
+        case outline
+        case striped
+    }
+    enum Color {
+        case pink
+        case purple
+        case green
+    }
+    enum Quantity: Int {
+        case single = 1
+        case double = 2
+        case triple = 3
+    }
+    
+    private func getShapeString() -> String {
+        switch shape {
+        case .triangle: return "▲"
+        case .circle: return "●"
+        case .square: return "■"
+        }
+    }
+    
+    private func getColorLiteral() -> UIColor {
+        switch color {
+        case .green: return #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+        case .purple: return #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        case .pink: return #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        }
+    }
     
     
-    // possible number of shapes in a card:
-    // 1, 2, 3
-    var numberOfShapes = 3 {
+    /* -- Initializers -- */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // todo - add
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        // todo - add
+    }
+    
+    
+    /* -- Propeties -- */
+    var numberOfShapes: Quantity = .double {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     } // is needs layout necessary?
     
-    // possible shapes:
-    // "▲", "●", "■"
-    var shape = "▲" {
+    var shape: Shape = .circle {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
 
-    // possible colors:
-    // #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
-    // #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-    // #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-    var color: UIColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1) {
+    var color: Color = .pink {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
     
-    // possible fillings:
-    // striped, outline, full
-    var filling = "full" {
+    var filling: Filling = .full {
         didSet {setNeedsDisplay(); setNeedsLayout()}
     }
     
     //
     private func attriubtedStringForCard() -> NSAttributedString? {
-//        private func attriubtedStringForCard(fontSize: CGFloat) -> NSAttributedString? {
-
+        
+        let cardColor = getColorLiteral()
+        let shapeString = getShapeString()
+        
         // create a preffered font with the given size.
         var font = UIFont.preferredFont(forTextStyle: .body)
 
@@ -52,20 +94,20 @@ class SetCardView: UIView
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
         
-        
         // Different filling types for cards get different alpha value for coloring.
-        let alpha = (filling == "striped") ? CGFloat(0.15): CGFloat(1.0)
+        let alpha = (filling.rawValue == "striped") ? CGFloat(0.15): CGFloat(1.0)
         let attributes: [NSAttributedString.Key: Any] = [
             // different filling types for cards get different .strokeWidth values.
-            .strokeWidth: (filling == "outline") ? 5.0 : -5.0,
-            .strokeColor: color,
-            .foregroundColor: color.withAlphaComponent(alpha),
+            .strokeWidth: (filling.rawValue == "outline") ? 5.0 : -5.0,
+            .strokeColor: cardColor,
+            .foregroundColor: cardColor.withAlphaComponent(alpha),
             .paragraphStyle: paragraphStyle,
             .font: font
             ]
+        
         var string = ""
-        for _ in 0..<numberOfShapes {
-            string += shape + " "
+        for _ in 0..<numberOfShapes.rawValue {
+            string += shapeString
         }
         return NSAttributedString(string: string, attributes: attributes)
     }
